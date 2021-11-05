99e321b776b15a2374a9f3ba53201aea80d5a5fe
Klecianny Melo <kleciannymelo@gmail.com>
2021-11-05 17:01:28 -0300

    Switch e case

---
+++ Módulo 1: Fundamentos do Desenvolvimento Web/Bloco 4: Introdução a JavaScript e Lógica de Programação/Dia-4.1/Switch-e-case.js
@@ -0,0 +1,19 @@
+let candidata = "ana";
+
+switch (candidata){
+    case "aprovada":
+        console.log("Você fo aprovado(a) no processo seletivo!");
+        break
+
+        case "lista":
+            console.log("Você está na lista de espera");
+            break 
+
+            case "reprovada":
+                console.log("Você foi reprovado(a)");
+                break
+
+    default:
+        console.log("não se aplica");
+        break
+}
