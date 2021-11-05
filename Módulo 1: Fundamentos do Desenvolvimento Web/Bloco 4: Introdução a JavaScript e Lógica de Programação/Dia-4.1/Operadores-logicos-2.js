04602299bcb6ec7bdee76a1adfb90cd6b1c1bc32
Klecianny Melo <kleciannymelo@gmail.com>
2021-11-05 16:39:26 -0300

    Operador OR: ||

---
+++ Módulo 1: Fundamentos do Desenvolvimento Web/Bloco 4: Introdução a JavaScript e Lógica de Programação/Dia-4.1/Operadores-logicos-2.js
@@ -0,0 +1,10 @@
+let weekDay = "sabado";
+let message = "";
+
+if (weekDay == "sabado" || weekDay == "domingo"){
+    message = "FINALMENTE, descanso merecido";
+} else {
+    message = "Oba, mais um dia de aprendizado na Trybe :D";
+}
+
+console.log(message);
