3305d8807c12399aad38abedfca7cae952dfc959
Klecianny Melo <kleciannymelo@gmail.com>
2021-11-09 11:06:35 -0300

    Exercício 12

---
+++ Módulo 1: Fundamentos do Desenvolvimento Web/Bloco 4: Introdução a JavaScript e Lógica de Programação/Dia-4.2/Exercicio-12.js
@@ -0,0 +1,7 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let indice = 0; indice < numbers.length; indice = indice + 1){
+    if (numbers[0] > numbers[indice]){
+        console.log(numbers[indice])
+    }
+}
