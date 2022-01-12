c9d5c8a02fc195a9fcdac0a39a7c0ce8abef8f47
Klecianny Melo <kleciannymelo@gmail.com>
2022-01-11 22:13:22 -0300

    Callback 4

---
+++ Módulo 1: Fundamentos do Desenvolvimento Web/Bloco 9 - JavaScript e Testes Assíncronos/Callback-4.js
@@ -0,0 +1,17 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+
+sendMarsTemperature = () => {
+  // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+  const temperature = getMarsTemperature();
+  setTimeout(() =>
+    console.log(`Mars temperature is: ${temperature} degree Celsius`), messageDelay());
+}; 
+
+sendMarsTemperature()
